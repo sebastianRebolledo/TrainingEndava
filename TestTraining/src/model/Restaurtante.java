9dadff16d068ab30615389d9704424b443bc0e52
Sebastian <seereme98@gmail.com>
2021-07-23 16:26:24 -0500

    upload changes: two atributes in class Restaurante: name and id

--- TestTraining/src/model/Restaurtante.java
+++ TestTraining/src/model/Restaurtante.java
@@ -2,4 +2,12 @@ package model;
 
 public class Restaurtante {
 
+    private String nameRestaurant;
+    private String idRestaurant;
+
+    public Restaurante(String nameRestaurant, String idRestaurant){
+        this.nameRestaurant=nameRestaurant;
+        this.idRestaurant=idRestaurant;
+    }
+
 }
